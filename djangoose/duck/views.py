97235a0a529dd55bf4d59847583e977014d22889
shiying <shiying.wei@ucl.ac.uk>
2025-02-10 14:45:31 +0000

    Add template code for response

--- djangoose/duck/views.py
+++ djangoose/duck/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Pokemon
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hi")
+
+def dynamic_data_view(request):
+    # Retrieve dynamic data from the model
+    pokemon = Pokemon.objects.all()
+
+     # Define a context dictionary variable and Pass the dynamic data to the template
+    context = {
+        'pokemon_name': pokemon.name,
+        'pokemon_movesets': pokemon.moveset.all(),
+        }
+
+    # Render the template with the dynamic data
+    return render(request, 'sample_template.html', context)
