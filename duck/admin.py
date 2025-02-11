bbccc3b49312b4aaae2849ba525c99785989bff0
shiying <shiying.wei@ucl.ac.uk>
2025-02-11 12:12:38 +0000

    Remove moveset

--- duck/admin.py
+++ duck/admin.py
@@ -1,22 +1,13 @@
 from django.contrib import admin
-from .models import Pokemon, Moveset, Move
+from .models import Pokemon, Move
 # Register your models here.
 
-@admin.register(Pokemon)
-class PokemonAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-
-@admin.register(Moveset)
-class MovesetAdmin(admin.ModelAdmin):
-
-    def safe_moveset(self, obj):
-        return str(obj.moveset.moves.all())
-    
-    list_display = (safe_moveset,)
-
-    
 
 @admin.register(Move)
 class MoveAdmin(admin.ModelAdmin):
     list_display = ("name",)
 #class MovesetAdmin(admin.ModelAdmin):
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ...
