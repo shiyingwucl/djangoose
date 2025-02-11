a4fbeefe4fa73944ac3a562416c3a6afa1763c11
shiying <shiying.wei@ucl.ac.uk>
2025-02-11 12:05:18 +0000

    Remove movesets - maybe its not need

--- duck/models.py
+++ duck/models.py
@@ -10,25 +10,18 @@ class Move(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+class Pokemon(models.Model):
 
-class Moveset(models.Model):
-
+    name = models.CharField(max_length=60,blank=False) 
+    # blank makes sure the property has to have a name, blank is admin, null is database
     first_move = models.ManyToManyField(Move,related_name="first_move")
     second_move = models.ManyToManyField(Move,related_name="second_move")
     third_move = models.ManyToManyField(Move,related_name="third_move")
     fourth_move = models.ManyToManyField(Move,related_name="fourth_move")
-
-    def __str__(self):
-        return f"{self.moveset.all()}"
-class Pokemon(models.Model):
-
-    name = models.CharField(max_length=60,blank=False) 
-    # blank makes sure the property has to have a name, blank is admin, null is database 
-    moveset = models.ManyToManyField(Moveset,related_name="moveset",)
     # figure out how to limit many to many relationships
     # when moves are = 4, set can_get_moves to False, <4 set to True
 
     def __str__(self):
-        return f"{self.name},{self.moveset.all()}"
+        return f"{self.name}"
     
     
