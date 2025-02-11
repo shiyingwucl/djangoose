ab9ecaf2839c9e25f524dab3e437ba668ce72ada
shiying <shiying.wei@ucl.ac.uk>
2025-02-11 12:22:18 +0000

    Allow pokemon to be created with empty moves

--- duck/models.py
+++ duck/models.py
@@ -14,10 +14,10 @@ class Pokemon(models.Model):
 
     name = models.CharField(max_length=60,blank=False) 
     # blank makes sure the property has to have a name, blank is admin, null is database
-    first_move = models.ManyToManyField(Move,related_name="first_move")
-    second_move = models.ManyToManyField(Move,related_name="second_move")
-    third_move = models.ManyToManyField(Move,related_name="third_move")
-    fourth_move = models.ManyToManyField(Move,related_name="fourth_move")
+    first_move = models.ManyToManyField(Move,related_name="first_move",blank=True)
+    second_move = models.ManyToManyField(Move,related_name="second_move",blank=True)
+    third_move = models.ManyToManyField(Move,related_name="third_move",blank=True)
+    fourth_move = models.ManyToManyField(Move,related_name="fourth_move",blank=True)
     # figure out how to limit many to many relationships
     # when moves are = 4, set can_get_moves to False, <4 set to True
 
