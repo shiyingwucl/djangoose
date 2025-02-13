963e4163157721a9b3288787a6273e5b5111431b
shiying <shiying.wei@ucl.ac.uk>
2025-02-13 13:44:14 +0000

    Add type class

--- duck/models.py
+++ duck/models.py
@@ -7,6 +7,7 @@ class Move(models.Model):
     name = models.CharField(max_length=60,blank=False)
     damage = models.IntegerField()
     accuracy = models.FloatField(max_length=4)
+    type = models.ForeignKey("Type",related_name="move_type",blank=False,on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         return self.name
@@ -14,10 +15,17 @@ class Move(models.Model):
 class Type(models.Model):
 
     name = models.CharField(max_length=30,blank=False)
-    
+
+#class TypeRelationship():
+#
+#    type = Type
+#    super_effective = list[Type]
+#    not_very_effective = list[Type]
+
 class Pokemon(models.Model):
 
     name = models.CharField(max_length=60,blank=False)
+    type = models.ForeignKey(Type,related_name="pokemon_type",blank=True,on_delete=models.SET_NULL, null=True)
     first_move = models.ForeignKey(Move,related_name="first_move",blank=True,on_delete=models.SET_NULL, null=True)
     second_move = models.ForeignKey(Move,related_name="second_move",blank=True,on_delete=models.SET_NULL, null=True)
     third_move = models.ForeignKey(Move,related_name="third_move",blank=True,on_delete=models.SET_NULL, null=True)
@@ -25,9 +33,7 @@ class Pokemon(models.Model):
     
     # blank makes sure the property has to have a name, blank is admin, null is database
     # figure out how to limit many to many relationships
-    # when moves are = 4, set can_get_moves to False, <4 set to True
 
     def __str__(self):
         return self.name
-    
     
