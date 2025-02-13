beb19b6086b6ef49f355afe0716db9da47f9ee69
shiying <shiying.wei@ucl.ac.uk>
2025-02-13 14:52:35 +0000

    Update admin displays to show attributes

--- duck/admin.py
+++ duck/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from .models import Pokemon, Move
+from .models import Pokemon, Move, Type
 # Register your models here.
 
 
 @admin.register(Move)
 class MoveAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-#class MovesetAdmin(admin.ModelAdmin):
+    list_display = ("name","type")
 @admin.register(Pokemon)
 class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("name","type","first_move","second_move","third_move","fourth_move")
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
     list_display = ("name",)
