0b763fd46f51b8719ab1c9fcf83e3dbd094db839
shiying <shiying.wei@ucl.ac.uk>
2025-02-17 12:31:06 +0000

    Display move's damage and accuracy in admin

--- duck/admin.py
+++ duck/admin.py
@@ -5,7 +5,7 @@ from .models import Pokemon, Move, Type
 
 @admin.register(Move)
 class MoveAdmin(admin.ModelAdmin):
-    list_display = ("name","type","pokemon_count")
+    list_display = ("name","type","damage","accuracy","pokemon_count")
 
     def pokemon_count(self, obj):
         return (obj.first_move.count() + 
